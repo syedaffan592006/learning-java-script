7b1b96c6b80672a7f094483f81a7117992871ba4
Syed Affan Asif <154745160+syedaffan592006@users.noreply.github.com>
2024-03-06 08:22:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let num1 = prompt("enter first number");
 let num2 = prompt("enter second number");
 num1 = Number(num1);
+num2 = Number(num2);
 console.log("addition", num1 + num2);
 let sum = num1 + num2;
 let minus = num1 - num2;
 alert("addition" + sum);
 alert("subtraction" + minus);
-// addition and subtraction 
+
